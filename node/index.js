7822dd3a751ba3f244b78509e4708aebf16d711d
rogerioLozano <rogerio.luis.lozano@hotmail.com>
2020-05-12 13:07:39 -0300

    implementação login

--- node/index.js
+++ node/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 const mysql = require('mysql');
-const { getPessoas, insertPessoas, deletePessoas, updatePessoas } = require('./api/controller.js');
+const { getPessoas, insertPessoas, deletePessoas, updatePessoas } = require('./api/pessoa/controller.js');
+const { createUserlogin,loginUser } = require('./api/login/controller.js');
+
 
 app.set('view engine','ejs');
 
@@ -12,6 +14,9 @@ app.get('/getPessoas/:id?',getPessoas);
 app.post('/insertPessoas',insertPessoas);
 app.delete('/deletePessoas/:id?',deletePessoas);
 app.put('/updatePessoas/:id?',updatePessoas);
+
+app.post('/create-login',createUserlogin);
+app.get('/login-user/:user?',loginUser);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
